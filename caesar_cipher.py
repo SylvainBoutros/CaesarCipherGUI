31baa65b0bce400f8a8ec3c0d8907b8ffec18b4b
Sylvain Boutros <128319204+SylvainBoutros@users.noreply.github.com>
2023-03-27 13:10:55 -0600

    Modified the layout of the cipher to be different

--- caesar_cipher.py
+++ caesar_cipher.py
@@ -14,35 +14,47 @@ class CaesarCipher:
         self.main_frame.place(relx=0.5, rely=0.5, anchor=CENTER)
         self.fonts = ("Arial", 16)
 
+        # The label saying "Encrypt Message" will be starting at column 0 and row 0, having a column span of 12
         self.encrypt_label = Label(self.main_frame, text="Encrypt Message:", font=self.fonts)
         self.encrypt_label.grid(column=0, row=0, sticky="nsew")
 
-        self.encrypt_display = Text(self.main_frame, font=self.fonts, height=10, width=5)
-        self.encrypt_display.grid(column=0, row=1, sticky="nsew")
+        # The encrypt display where we can type a message in plain text should start at column 0 and row 1
+        # It will have a height of 5 and a width of 12
+        self.encrypt_display = Text(self.main_frame, font=self.fonts, height=5, width=40)
+        self.encrypt_display.grid(column=0, row=1, columnspan=12)#, sticky="nsew")
 
-        self.key_label = Label(self.main_frame, text="Key:", font=self.fonts)
-        self.key_label.grid(column=6, row=0)
-
-        self.key = Spinbox(self.main_frame, font=self.fonts, values=self.minmax)
-        self.key.grid(column=6, row=1)
-
+        # The label saying "Decrypt Messgae" will be starting at column 0 and row 6, having a column span of 12 as well
         self.decrypt_label = Label(self.main_frame, text="Decrypt Message:", font=self.fonts)
-        self.decrypt_label.grid(column=7, row=0, sticky="nsew")
+        self.decrypt_label.grid(column=0, row=6, sticky="nsew")
 
-        self.decrypt_display = Text(self.main_frame, font=self.fonts, height=10, width=5)
-        self.decrypt_display.grid(column=7, row=1, sticky="nsew")
+        # The decrypt display where we can paste a message that was 'encrypted' should start at column 0 and row 7 
+        # It will have a height of 5 and width of 12
+        self.decrypt_display = Text(self.main_frame, font=self.fonts, height=5, width=40)
+        self.decrypt_display.grid(column=0, row=7, columnspan=12)#, sticky="nsew")
+        
+        # The secret key label will be starting at column 0 and row 12, having a column span of half the display 6
+        self.key_label = Label(self.main_frame, text="Key:", font=self.fonts)
+        self.key_label.grid(column=0, row=12, columnspan=6)
 
+        # The spinbox where we can adjust the number for the secret key will be taking the remaining half of that display
+        self.key = Spinbox(self.main_frame, font=self.fonts, values=self.minmax)
+        self.key.grid(column=6, row=12, columnspan=6)
+
+        # The encrypt button will be starting on column 0 and row 13
         self.btn_encrypt = Button(self.main_frame, text="Encrypt", font=self.fonts, command=lambda: self.get_message("encrypt"))
-        self.btn_encrypt.grid(column=0, row=2, columnspan=12, sticky="nsew")
+        self.btn_encrypt.grid(column=0, row=13, columnspan=6, sticky="nsew")
 
+        # The decrypt button will be starting on column 6 and row 13
         self.btn_decrypt = Button(self.main_frame, text="Decrypt", font=self.fonts, command=lambda: self.get_message("decrypt"))
-        self.btn_decrypt.grid(column=0, row=3, columnspan=12, sticky="nsew")
+        self.btn_decrypt.grid(column=6, row=13, columnspan=6, sticky="nsew")
 
+        # The clear button will be starting on column 0 and row 14
         self.btn_clear = Button(self.main_frame, text="Clear Screen", font=self.fonts, command=self.clear_screen)
-        self.btn_clear.grid(column=0, row=4, columnspan=12, sticky="nsew")
-
-        self.btn_quit = Button(self.main_frame, text="Exit", font=self.fonts, command=self.safely_exit)
-        self.btn_quit.grid(column=0, row=9, columnspan=12, sticky="nsew")
+        self.btn_clear.grid(column=0, row=14, columnspan=6, sticky="nsew")
+        
+        # The quit button will be starting on column 6 and row 14
+        self.btn_quit = Button(self.main_frame, text="Exit", bg="red", font=self.fonts, command=self.safely_exit)
+        self.btn_quit.grid(column=6, row=14, columnspan=6, sticky="nsew")
 
         self.types = None
         self.results = ""
